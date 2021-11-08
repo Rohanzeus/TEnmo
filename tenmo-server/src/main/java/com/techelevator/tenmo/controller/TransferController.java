3c388dce65b67943ea998f341a022a1abc831f16
Roman Oliver <oliv1289@gmail.com>
2021-11-07 21:24:45 -0500

    Tried to add the create boolean to TransferDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -5,6 +5,7 @@ import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.TransferDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class TransferController {
     private TransferDao transferDao;
 //    private UserDao userDao;
 
-
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "transfer", method = RequestMethod.POST)
     public String makeTransfer(@RequestBody TransferDTO transfer) {
         return transferDao.sendTransfer(transfer.getFromAccount(), transfer.getToAccount(),transfer.getAmountToOrFrom());
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -7,4 +7,5 @@ import java.math.BigDecimal;
 public interface TransferDao {
     public String sendTransfer(int fromUser, int toUser, BigDecimal amount);
 
+//    boolean create(int fromUser, int toUser, BigDecimal amount);
 }
